d5511ad669a808804c8053d61b8ac0f31932f073
Simon Kim <09simon@naver.com>
2024-06-26 00:28:24 +0900

    A few cahanges

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ total_times = 0
 success_times = 0
 possibility = 0
 value = [0,0,0,0,0]
-
-if input("To set to default, type 'default'. Otherwise, type anything else to set manually : ") == 'default':
+a =input("Do you want to set to default? (Y/N) : ")
+if a == 'Y':
   file = open("default.txt", "r")
   default_values = [line.rstrip() for line in file]
   print(default_values)
   for i in range(0, 5):
-    value[i] = int(default_values[i])
-  value[4] /= 100
+    value[i] = float(default_values[i])
+elif a=='N':
+  value[0] = float(input("Enter your start point. : "))
+  value[1] = float(input("Enter your target point. : "))
+  value[2] = float(input("How many points do you want to get if you win? : "))   
+  value[3] = float(input("How many points do you want to lose if you lose? : "))   
+  value[4] = float(input('Set the odds that you will win (between 0 and 1) : '))
 else:
-  value[0] = int(input("Enter your start point. : "))
-  value[1] = int(input("Enter your target point. : "))
-  value[2] = int(input("How many points do you want to get if you win? : "))   
-  value[3] = int(input("How many points do you want to lose if you lose? : "))   
-  value[4] = int(input('Set the odds that you will win (between 0 and 100) : ')) / 100
+  print("enter Y or N")
+  exit()
 while True:
   balance = value[0]
   while True:
